770f67d952fafb50b204864ed15052ebabc2abf4
MGniew <218138@student.pwr.edu.pl>
2017-08-02 21:41:43 +0200

    Issue completed

--- light.h
+++ light.h
@@ -6,12 +6,23 @@ class Light
 {
 public:
     Light();
+    Light(float* pos, float* amb, float* dif, float* spec);
     ~Light();
 
     float* pos;
     float* amb;
     float* dif;
     float* spec;
+
+    float *getPos();
+    float *getAmb();
+    float *getDif();
+    float *getSpec();
+    void setPos(float x, float y, float z);
+    void setAmb(float r, float g, float b);
+    void setDif(float r, float g, float b);
+    void setSpec(float r, float g, float b);
+
 };
 
 #endif // LIGHT_H
